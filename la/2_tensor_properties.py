f2acdc5c1edfcf33c09e0e70283bb3edfe6a1091
yuro <zerone.taken@gmail.com>
2025-10-22 09:14:23 +0000

    tensor property 'ranks' implemented

--- la/2_tensor_properties.py
+++ la/2_tensor_properties.py
@@ -1,3 +1,16 @@
 import torch
 
-#PROPERTIES OF TENSORS
+# TOPIC : PROPERTIES OF TENSORS
+
+s = torch.tensor(25)  # SCALAR
+v = torch.tensor([10, 20, 30])  # VECTOR
+m = torch.tensor([[1, 2, 3], [4, 5, 6]])  # MATRIX
+
+# 1. RANKS : It's the simplest property, "rank" is the number of dimensions of a tensor.
+# NDIM = Number of Dimensions
+print(f"Scalar rank : {s.ndim}")
+print(f"Vector rank : {v.ndim}")
+print(f"Matrix rank : {m.ndim}")
+
+
+# 2. SHAPE
