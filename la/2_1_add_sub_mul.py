e3288bd78ed04f720a52bbdde041601e772e1221
yuro <zerone.taken@gmail.com>
2025-10-23 10:55:48 +0000

    implemented add/sub and hadamard product

--- la/2_1_add_sub_mul.py
+++ la/2_1_add_sub_mul.py
@@ -1,1 +1,17 @@
 import torch
+
+
+a = torch.tensor([[1, 2], [3, 4]])
+b = torch.tensor([[10, 20], [30, 40]])
+
+# 1.. ADDITION/SUBSTRACTION
+print(f"Tensor 1 :-\n{a}")
+print(f"Tensor 2 :-\n{b}")
+print("")
+print(f"Add :\n {a + b}")
+print(f"Sub :\n {a - b}")
+print("")
+
+# 2. ELEMENTAL MULTIPLICATION(OR HADAMARD PRODUCT)
+print(f"Hadamard elemental product :\n{a * b}")
+print("")
