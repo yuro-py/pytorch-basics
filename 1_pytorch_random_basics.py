cabdf971ba6ad9875313d3fa8ef8218e2b8f17f7
yuro <zerone.taken@gmail.com>
2025-10-28 13:14:00 +0000

    moved full chap0 to file1 of pytorch. chap1 will be in file2. shall I rename files to file0, file1 etc?

--- 1_pytorch_random_basics.py
+++ 1_pytorch_random_basics.py
@@ -4,7 +4,7 @@
 # https://youtu.be/V_xro1bcAuA?si=fkVRlmOqcXsu7FBr following this video
 
 import torch
-# import numpy as np
+import numpy as np
 # import pandas as pd
 # import matplotlib.pyplot as plt
 
@@ -65,12 +65,52 @@ import torch
 # ---------------------------------------------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------------------------------------------
-# 1. INDEXING/SLICING :-
+# INDEXING/SLICING :-
 t = torch.tensor([[1, 2, 3, 4], [5, 6, 7, 8]])
-print(f"INDEXING/SLICING on this tensor :-\n{t}\n\n")
-print(f"Getting a single element -> {t[0, 1].item()}\n\n")
-print(f"Getting 0th row ->{t[0, :]}\n\n")
-print(f"All rows specific columns ->{t[:, 2]}\n\n")
-print(f"Specific rows all columsn :-\n{t[0, :]}\n\n")
-print(f"Sub matrix :-\n{t[0:2, 0:0]}")  # 3 7 1 5
-print(f"Negative indexing -> \n{t[-1]}")
+# print(f"INDEXING/SLICING on this tensor :-\n{t}\n\n")
+# print(f"Getting a single element -> {t[0, 1].item()}\n\n")
+# print(f"Getting 0th row ->{t[0, :]}\n\n")
+# print(f"All rows specific columns ->{t[:, 2]}\n\n")
+# print(f"Specific rows all columsn :-\n{t[0, :]}\n\n")
+# print(f"Sub matrix :-\n{t[0:2, 0:0]}")  # 3 7 1 5
+# print(f"Negative indexing -> \n{t[-1]}")
+
+# ---------------------------------------------------------------------------------------------------------------
+# 1. "NUMPY TO TENSOR" and "TENSOR TO NUMPY"
+# a.
+array = np.arange(1.0, 10.0)
+tensor = torch.from_numpy(array)  # THIS!
+# after conversion, specify any type using eg ".type(torch.float32)" you want in the last
+# print(array, tensor)
+# print(array.shape, tensor.shape)
+# b.
+tensor = torch.ones(9)
+array = tensor.numpy()  # THIS!
+# print(tensor, array)
+# print(tensor.shape, array.shape)
+
+
+# 2. REPRODUCIBILITY : trying to take a random out of a random
+# NOTES -> HOW A NEURAL NETWORK LEARNS :-
+# STARTS WITH RANDOM NUMS -> TENSOR OPS -> UPDATE RANDOM NUMBERS TO MAKE THEM A BETTER REPRESENTATIONS OF THE DATA -> REPEAT...
+# TO REDUCE RANDOMNESS EACH TIME : INTRODUCE A "RANDOM SEED"
+torch.manual_seed(42)
+t1 = torch.rand(3, 4)
+torch.manual_seed(42)
+# RESET THE SEED BEFORE EACH NEW RANDOM VARIABLE TO ACHIEVE REPRODUCIBILITY
+t2 = torch.rand(3, 4)
+# print(t1)
+# print(t2)
+# print(t1 == t2)
+
+# 3. CHECK GPU AVAILABILITY THROUGH PYTORCH
+# a. check availability
+# print(torch.cuda.is_available())
+# b. device agnostic setup
+device = "cuda" if torch.cuda.is_available() else "cpu"
+# print(device)
+# in future there will be moments where we use this device variable
+# c. count gpus
+# print(torch.cuda.device_count())
+# d. lot more weird gpu accessing methods. will reference that section later.
+# ---------------------------------------------------------------------------------------------------------------
