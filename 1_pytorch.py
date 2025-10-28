ac382f074704911670b3688651a5be4fc5c46dbd
yuro <zerone.taken@gmail.com>
2025-10-28 09:19:47 +0000

    played with shapes and dimensions. reshaping, view, stacking. cool stuff ngl

--- 1_pytorch.py
+++ 1_pytorch.py
@@ -16,5 +16,38 @@ a = torch.tensor([2, 2], dtype=torch.int32, device="cpu", requires_grad=False)
 # use http://matrixmultiplication.xyz/ to visualize
 # --------------------------------------------------------
 
+# --------------------------------------------------------
 # for reducing the size of the tensor : sum, mean, min, max; can specify the dim asw.
 # for finding the index quantity use "var.argmax()" or "var.argmin()"
+# --------------------------------------------------------
+
+# --------------------------------------------------------
+# Manipulating shapes/dimensions :-
+# 1. reshaping/adding dimensions : reshapes a tensor. for eg : shape(1,10) can be "reshape(9,1)".
+# x = torch.arange(1, 10)  # 1-9
+x = torch.tensor([[1, 2], [3, 4], [5, 6]])
+print(x)
+# xr = x.reshape(1, 9)
+# print(xr)  # print(x.shape, xr.shape)
+print()
+
+# 2. view : without creating a new one in memory, returns another reshaped view of an input tensor.
+# z = x.view(1, 9)  # same as reshape using x's memory. hence, changing z changes x.
+# print(z)
+
+# 3. stacking : combine multiple tensors on top. torch.stack(for basic stack), torch.vstack(for vertical) and torch.hstack(for side by side).
+x_stack = torch.stack(
+    [x, x, x, x], dim=2
+)  # IMPORTANT : the higher the dim value, the inner the brackets go. lowest dim = outermost bracket.
+print(x_stack)
+# print(x.shape)
+# print()
+
+# 4. squeeze :
+# print()
+
+# 5. unsqueeze :
+# print()
+
+# 6. permute : returns a view of tensor with permuted dimensions.
+# print()
