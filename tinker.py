f014050e5a64fa073e59b6c52eba4c450c106b74
yuro <zerone.taken@gmail.com>
2025-10-29 20:00:52 +0000

    testing a codeblock from thinkingmachine nondeterminism paper

---
+++ tinker.py
@@ -0,0 +1,7 @@
+import torch
+
+A = torch.randn(2048, 2048, device="cuda", dtype=torch.bfloat16)
+B = torch.randn(2048, 2048, device="cuda", dtype=torch.bfloat16)
+ref = torch.mm(A, B)
+for _ in range(1000):
+    assert (torch.mm(A, B) - ref).abs().max().item() == 0
